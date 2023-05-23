909118bdba2fb4df5675566ed8914b5be4ab0d72
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-23 11:20:36 +0800

    Controller
    Controller注解案例

---
+++ 常用注解/Controller.java
@@ -0,0 +1,11 @@
+/*
+Author:steam-404
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//标记一个类作为Spring MVC控制器，用于处理HTTP请求和响应。
+public class Controller {
+    
+}
